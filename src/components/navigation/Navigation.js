5640e4452d2aed083167fe47fef7e335a0f0a06c
Simon <rejmus.simon990@gmail.com>
2023-05-26 21:39:31 +0200

    First commit

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from '../../logo.png';
+import { TbSearch } from 'react-icons/tb';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <div className="search-bar">
+                <TbSearch className="search-icon"/>
+                <input type="text" className="search-input"/>
+            </div>
+            <div className="logo">
+                <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div className="navigation-links">
+                <a href="/" className="nav-link active">Home</a>
+                <a href="/contact" className="nav-link">Contact</a>
+                <a href="/about" className="nav-link">About me</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation;
